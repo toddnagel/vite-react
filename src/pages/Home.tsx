76fbb58786fe248f49f18264883884b6c77d6515
Todd Nagel <tnagel@gloo.us>
2026-02-03 01:59:52 -0800

    initial setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import TabbedContent from "../components/TabbedContent";
 
 function Home() {
