199f60205bd0b6ec0b1565a3fde2abc7f2c23731
Todd Nagel <todd.alan.nagel@gmail.com>
2026-03-09 17:25:04 -0700

    test

--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -1,11 +1,6 @@
-import GsapHeroParallax from "../components/GsapHeroParallax";
-import GsapPageContent from "../components/GsapPageContent";
 import GsapPageSubHeading from "../components/GsapPageSubHeading";
-//import SectionParallaxBlobs from "../components/SectionParallaxBlobs";
-//import { useSectionParallaxOffsets } from "../hooks/useSectionParallaxOffsets";
 
 function TermsAndConditions() {
-  //const { sectionRef, bgShapeOffset, colorBgOffset, colorBg2Offset } = useSectionParallaxOffsets();
 
   return (
     <>
@@ -13,14 +8,7 @@ function TermsAndConditions() {
         className="relative bg-[url('/Pattern.png')] bg-cover bg-center bg-no-repeat pt-16 pb-4 md:pt-30 border-b border-[#36e9e424]"
       >
       </section>
-      <section
-        //ref={sectionRef}
-        className="relative overflow-hidden py-8 lg:py-12 bg-[var(--bg)]">
-        {/* <SectionParallaxBlobs
-          bgShapeOffset={bgShapeOffset}
-          colorBgOffset={colorBgOffset}
-          colorBg2Offset={colorBg2Offset}
-        /> */}
+      <section className="relative overflow-hidden py-8 lg:py-12 bg-[var(--bg)]">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-4xl mx-auto [&_p]:text-[#decee9] [&_ul]:text-[#decee9] [&_li]:text-[#decee9] [&_li>span]:text-[#decee9]">
             <GsapPageSubHeading heading="Terms & Conditions" />
